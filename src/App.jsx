ced67fbe7fda96c90f6cc1c61ec8a6ca0150d1bd
German <germanlemos94@gmail.com>
2024-10-05 19:59:06 -0300

    feature/Products

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Products from "./components/Products/Products";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
